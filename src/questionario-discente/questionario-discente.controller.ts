a5a0b7455285d4d706107884ebe3c0660f363e4e
fabriciofsn <banksapk@gmail.com>
2024-09-08 17:01:55 -0300

    auth form discente

--- src/questionario-discente/questionario-discente.controller.ts
+++ src/questionario-discente/questionario-discente.controller.ts
@@ -1,33 +1,62 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { QuestionarioDiscenteService } from './questionario-discente.service';
 import { CreateQuestionarioDiscenteDto } from './dto/create-questionario-discente.dto';
 import { UpdateQuestionarioDiscenteDto } from './dto/update-questionario-discente.dto';
+import { Public } from 'src/decorator/public.decorator';
+import { Roles } from 'src/decorator/role.decorator';
+import { Role } from 'src/auth/role.enum';
 
 @Controller('questionario-discente')
 export class QuestionarioDiscenteController {
-  constructor(private readonly questionarioDiscenteService: QuestionarioDiscenteService) {}
+  constructor(
+    private readonly questionarioDiscenteService: QuestionarioDiscenteService,
+  ) {}
 
+  @Public()
+  @HttpCode(HttpStatus.OK)
   @Post()
   create(@Body() createQuestionarioDiscenteDto: CreateQuestionarioDiscenteDto) {
-    return this.questionarioDiscenteService.create(createQuestionarioDiscenteDto);
+    return this.questionarioDiscenteService.create(
+      createQuestionarioDiscenteDto,
+    );
   }
 
   @Get()
+  @Roles(Role.Napne)
   findAll() {
     return this.questionarioDiscenteService.findAll();
   }
 
   @Get(':id')
+  @Roles(Role.Napne)
   findOne(@Param('id') id: string) {
     return this.questionarioDiscenteService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateQuestionarioDiscenteDto: UpdateQuestionarioDiscenteDto) {
-    return this.questionarioDiscenteService.update(+id, updateQuestionarioDiscenteDto);
+  @Roles(Role.Napne)
+  update(
+    @Param('id') id: string,
+    @Body() updateQuestionarioDiscenteDto: UpdateQuestionarioDiscenteDto,
+  ) {
+    return this.questionarioDiscenteService.update(
+      +id,
+      updateQuestionarioDiscenteDto,
+    );
   }
 
   @Delete(':id')
+  @Roles(Role.Napne)
   remove(@Param('id') id: string) {
     return this.questionarioDiscenteService.remove(+id);
   }
